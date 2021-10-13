406d17383624b2fe78021bf30ccbdd23bbf15693
Sworld <1628767411@qq.com>
2021-10-14 00:19:13 +0800

    重构函数,优化可读性

--- res/JsTools/clock.js
+++ res/JsTools/clock.js
@@ -1,4 +1,151 @@
-import {getTops, sendFailureWork, sendSuccessWork} from "./message";
+function get_name_psw() {
+    // need Encryption 需要加密
+    let accountName = document.getElementById("accountName").value
+    let accountPsw = document.getElementById("accountPsw").value
+    return [accountName, accountPsw]
+}
+
+function getXmlhttp() {
+    let xmlhttp;
+    xmlhttp = new XMLHttpRequest();
+    return xmlhttp
+}
+
+function sendData(operation, amount) {
+    let name = get_name_psw()[0]
+    let psw = get_name_psw()[1]
+    let xmlhttp = getXmlhttp();
+    let url = "app/?operation=" + operation +
+        "&name=" + name +
+        "&psw=" + psw +
+        "&amount=" + amount;
+    xmlhttp.open("GET", url);
+    console.log(url)
+    return xmlhttp
+}
+
+function getTops() {
+    console.log("getTops()")
+    let xmlhttp = getXmlhttp()
+    xmlhttp.open("GET", "/database/tops")
+    xmlhttp.onreadystatechange = function () {
+        let cur_time;
+        let cur_name;
+        let res;
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            let tops = document.getElementById("tops")
+            tops.innerHTML = ""
+            res = JSON.parse(xmlhttp.response)
+            for (let i = 0; i < res.length; i++) {
+                ({NAME: cur_name, TIME: cur_time} = res[i]);
+                tops.innerHTML += "<li>" + cur_name + " 一共工作了 " + cur_time + " 分钟</li>"
+            }
+        }
+    }
+    xmlhttp.send()
+}
+
+function sendSuccessWork(work) {
+    console.log("正在向服务器汇报工作时间")
+    let xmlhttp = sendData("addTime", work)
+    xmlhttp.send()
+}
+
+function sendFailureWork() {
+    console.log("正在向服务器汇报失败次数")
+    let xmlhttp = sendData("addFails", 1)
+    xmlhttp.send()
+}
+
+/**
+ *Register an account.
+ *Name & Psw are from the HTML input.
+ * @return {string} error message
+ */
+function regAccount() {
+    console.log("regAccount()")
+    let accountName = get_name_psw()[0]
+    let accountPsw = get_name_psw()[1]
+    let regPos = /^[A-Za-z0-9_]{4,10}$/
+    if (!regPos.test(accountName) || accountName.length <= 4) {
+        alert("请不要输入英文和数字以外的字符，昵称长度请大于4小于10")
+        return "err"
+    }
+    if (accountPsw.length <= 4) {
+        alert("密码长度请大于4")
+        return "err"
+    }
+    let xmlhttp = getXmlhttp()
+    xmlhttp.open("GET", "/reg/" + accountName + "/" + accountPsw)
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            switch (xmlhttp.responseText) {
+                case "success":
+                    alert("注册成功\n需要使用的话只需要输入账号密码，不需要登陆")
+                    break
+                case "used name":
+                    alert("昵称被占用")
+                    break
+                default:
+                    alert("未知错误")
+            }
+        }
+    }
+    xmlhttp.send()
+
+}
+
+/**
+ *Get your score
+ */
+function getStatistics() {
+    console.log("getStatistics()")
+    let accountName = get_name_psw()[0]
+    let accountPsw = get_name_psw()[1]
+    let xmlhttp = getXmlhttp()
+    xmlhttp.open("GET", "/getSta/" + accountName + "/" + accountPsw)
+    xmlhttp.onreadystatechange = function () {
+        let res = xmlhttp.responseText
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            if (res !== "fail") {
+                res = JSON.parse(res)
+                let {WINS, TIME, FAILS} = res;
+                document.getElementById("wins").innerHTML = "成功次数:" + WINS
+                document.getElementById("fails").innerHTML = "失败次数:" + FAILS
+                document.getElementById("time").innerHTML = "完成时间:" + TIME
+            } else {
+                alert("出现错误，请检查密码和账号是否正确\n也可能是服务器问题")
+            }
+        }
+    }
+    xmlhttp.send()
+}
+
+/**
+ *Delete a account.
+ */
+function delAccount() {
+    if (!window.confirm("注意，如果选择确定将会删除您的账号")) {
+        console.log("取消删除账号")
+        return
+    }
+    console.log("delAccount()")
+    let accountName = get_name_psw()[0]
+    let accountPsw = get_name_psw()[1]
+    let xmlhttp = getXmlhttp()
+    xmlhttp.open("GET", "/del/" + accountName + "/" + accountPsw)
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            if (xmlhttp.responseText === "success") {
+                alert("删除账号成功")
+            } else {
+                alert("删除账号失败，请检查账号密码是否正确\n也可能是服务器错误")
+            }
+        }
+    }
+    xmlhttp.send()
+}
+
 
 
 /**
@@ -66,6 +213,8 @@ function startState(state){
     switch (state) {
         case "work":
             titleText = "Working"
+            buttonText = "Stop"
+            buttonFunc = userStopClock
             break;
         case "rest":
             titleText = "Resting"
@@ -105,10 +254,11 @@ function secondTimer(work, rest, times) {
             let curSeconds = totalSeconds % eachTotalSeconds // rest of time
             if (totalSeconds <= 0) {
                 clearInterval(window.timer)
-                changeToState("done","番茄钟结束")
+                changeToState("normal","番茄钟结束")
             }
             if (curSeconds === restSeconds) {
-                changeToState("rest","进入休息",work)
+                sendSuccessWork(work)
+                changeToState("rest","进入休息")
             }
             if (curSeconds === 0 && totalSeconds > 0) {
                 curSeconds = eachTotalSeconds
@@ -126,34 +276,16 @@ function secondTimer(work, rest, times) {
                 print_time(minutes_seconds.minutes, minutes_seconds.seconds)
             }
             totalSeconds--
-        }, 1000)
+        }, 10)
 }
 
 /**
  *
- * @param {string} state work,rest,done,fail
+ * @param {string} state work,rest,normal,fail
  * @param {string} message the massage to alert and log
- * @param {number} work work time (minute)
  * */
-function changeToState(state,message,work=0){
-    switch (state){
-        case "work":
-            startState("work")
-            break
-        case "rest":
-            sendSuccessWork(work)
-            startState("rest")
-            break
-        case "done":
-            startState("normal")
-            break
-        case "fail":
-            clearInterval(window.timer)
-            startState("normal")
-            print_time(0, 0)
-            sendFailureWork()
-            break
-    }
+function changeToState(state,message){
+    startState(state)
     console.log(message)
     alert(message)
 }
@@ -173,7 +305,10 @@ function startClock() {
 }
 
 function userStopClock() {
-    changeToState("fail","放弃了番茄钟")
+    clearInterval(window.timer)
+    changeToState("normal","放弃了番茄钟")
+    print_time(0, 0)
+    sendFailureWork()
 }
 
 window.onload = function () {
